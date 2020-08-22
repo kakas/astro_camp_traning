7d40767f13722a5f8dd57c9217c30623b01223b6
kakas <shrimptrain@gmail.com>
2020-08-22 18:46:09 +0800

    Add: task CRUD

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,48 @@
+# frozen_string_literal: true
+
 class TasksController < ApplicationController
+  before_action :load_task, only: %i[show update destroy]
+
+  def index
+    # TODO: use kaminari
+    render json: Task.all.select(Task::VIEWABLE_COLUMNS)
+  end
+
+  def create
+    @task = Task.create(task_params)
+
+    if @task
+      render json: @task.as_json(only: Task::VIEWABLE_COLUMNS), status: :created
+    else
+      render json: @task.errors
+    end
+  end
+
+  def show
+    render json: @task.as_json(only: Task::VIEWABLE_COLUMNS)
+  end
+
+  def update
+    if @task.update(task_params)
+      render json: @task, status: :ok
+    else
+      render json: @task.errors
+    end
+  end
+
+  def destroy
+    @task.destroy
+    head :no_content
+  end
+
+  private
+
+  def load_task
+    @task = Task.find_by(id: params[:id])
+    head :not_found unless @task
+  end
+
+  def task_params
+    params.require(:task).permit(Task::EDITABLE_COLUMNS)
+  end
 end
--- spec/requests/tasks_request_spec.rb
+++ spec/requests/tasks_request_spec.rb
@@ -1,4 +1,59 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe 'Tasks', type: :request do
+  describe 'get all tasks' do
+    it do
+      tasks = create_list(:task, 2)
+      get tasks_path
+      expect(response).to have_http_status(:success)
+      expect(response.body).to include(tasks[0].title)
+      expect(response.body).to include(tasks[1].title)
+      expect(JSON.parse(response.body).size).to eq(2)
+    end
+  end
+
+  describe 'create task' do
+    it do
+      task_params = { task: attributes_for(:task) }
+      expect{ post tasks_path, params: task_params}.to change{ Task.count }.by(1)
+      expect(response).to have_http_status(:created)
+      expect(response.body).to include(task_params[:task][:title])
+    end
+  end
+
+  describe 'get task' do
+    let(:task){ create(:task) }
+
+    it do
+      get task_path(task.id)
+      expect(response).to have_http_status(:success)
+      expect(JSON.parse(response.body)['title']).to eq(task.title)
+    end
+
+    context 'when record not found' do
+      it do
+        get task_path(-1)
+        expect(response).to have_http_status(:not_found)
+      end
+    end
+  end
+
+  describe 'update task' do
+    it do
+      task = create(:task)
+      task_params = { task: { title: 'new-title' } }
+      expect{ patch task_path(task.id), params: task_params }.to change{ task.reload.title }.to('new-title')
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe 'destroy task' do
+    it do
+      task = create(:task)
+      expect{ delete task_path(task.id) }.to change{ Task.count }.by(-1)
+      expect(response).to have_http_status(:no_content)
+    end
+  end
 end
