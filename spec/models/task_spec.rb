688f0ac7a2554db0f726d23e7393e7b84a81b774
kakas <shrimptrain@gmail.com>
2020-08-30 14:33:15 +0800

    Add: task start_time and end_time validation

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,48 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validate #end_date_should_after_start_date' do
+    subject(:task) { build(:task, start_time: start_time, end_time: end_time) }
+
+    context 'when start_date is blank' do
+      let(:start_time) { nil }
+    end
+
+    context 'when end_date is blank' do
+      let(:end_time) { nil }
+    end
+
+    context 'when start_date and end_date both exists' do
+      context 'when start_date greater then end_date' do
+        let(:start_time) { Time.zone.now }
+        let(:end_time) { start_time - 1.second }
+
+        it do
+          expect(task.valid?).to eq(false)
+          expect(task.errors.messages[:start_time]).to include('start time should less than end time')
+        end
+      end
+
+      context 'when start_date equal to end_date' do
+        let(:start_time) { Time.zone.now }
+        let(:end_time) { start_time }
+
+        it do
+          expect(task.valid?).to eq(false)
+          expect(task.errors.messages[:start_time]).to include('start time should less than end time')
+        end
+      end
+
+      context 'when start_date less then end_date' do
+        let(:start_time) { Time.zone.now }
+        let(:end_time) { start_time + 1.second }
+
+        it do
+          expect(task.valid?).to eq(true)
+        end
+      end
+    end
+  end
 end
